bee27553c7cba97de19040d67bf788ac0644ee1c
Asiah Eskridge <aeskridge0@gmail.com>
2021-06-29 14:36:07 -0700

    uploading lab files

---
+++ Desktop/HtH-Program-Assignments/dictionaries_practice.py
@@ -0,0 +1,1 @@
+grocery_list = {chicken: 1.59, beef:1.99, cheese:1.00, milk:2.50}
---
+++ Desktop/HtH-Program-Assignments/fundamentals_practice.py
@@ -0,0 +1,16 @@
+print("Hello World")
+# Ask for GitHub help.
+
+
+name = "LeBron"
+age = 35
+salary = 37.44
+plays_basketball = True
+jersey_number = "23"
+
+car_name = "Lexus"
+x = 50
+my_first_name = "Asiah"
+
+type(car_name)
+print(car_name)
